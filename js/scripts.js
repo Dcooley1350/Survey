0e2a80b6f136a06c5c3a6d8b84d933320bcbd952
Devin Cooley <dcooley1350@gmail.com>
2019-06-04 15:07:42 -0700

    add script and css style to radios

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#blank-form form").submit(function(event) {
+    var firstNameInput = $("input#firstName").val();
+    var lastNameInput = $("input#lastName").val();
+    var ageInput = $("input#age").val();
+    var dob = $("#born").val();
+    var favoriteColor =$("input#color").val();
+    var favoriteMusic =$("input#music").val();
+    var favoriteFood =$("input#food").val();
+    var favoriteSeason =$("#season").val;
+  })
+})
